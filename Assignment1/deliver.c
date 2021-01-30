9ecf730d9f45a29e9cf1c9939c671279f5e3ed6a
Robert Chinezon <r.chinezon@mail.utoronto.ca>
2021-01-30 12:52:33 -0500

    Created deliver

---
+++ Assignment1/deliver.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main( int argc, char *argv[] )
+{
+    char * serverAddress = argv[1];
+    char * serverPortNum= argv[2];
+    //-----------------------------------------------------------------------------------------------------------
+
+    return 0;
+}
