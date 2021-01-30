e1182f88cf619dbbb4aca6b898157e21e334a679
Navid Pour <Navidkpour@gmail.com>
2021-01-30 12:32:24 -0500

    Update server.c
    added some basic erver side code

---
+++ Assignment1/server.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#define MYPORT "3490"  // the port users will be connecting to
+#define BACKLOG 10 
+
+int main() {
+    struct sockaddr_storage client_addr;
+    socklen_t addr_size;
+    struct addrinfo hints, *res;
+    int sockfd, new_fd;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    getaddrinfo(NULL, MYPORT, &hints, &res);
+
+    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    if(sockfd < 0){
+        printf("Error while creating socket\n");
+        return -1;
+    }
+    printf("Socket created successfully\n");
+    
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) < 0) {
+        printf("Couldn't bind to the port\n");
+        return -1;
+    }
+
+    printf("Done with binding\n");
+    printf("Listening for incoming messages...\n\n");
+    
+    listen(sockfd, BACKLOG);
+
+
+    addr_size = sizeof client_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+
+
+
+    if (recvfrom(sockfd, client_message, sizeof(client_message), 0,
+            (struct sockaddr*)&client_addr, &client_struct_length) < 0){
+            printf("Couldn't receive\n");
+            return -1;
+        }
+
+    close(sockfd);
+}
